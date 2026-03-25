e0e1fd78e7d4bb0f748f4404c05289a13b62f41f
Ishan Dilmith <168458597+IshanDilmith@users.noreply.github.com>
2026-03-25 15:31:16 +0530

    feat: add Nginx configuration for serving React app and update Vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,12 @@
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-  const apiUrl = env.VITE_API_BASE_URL;
-
-  return {
-    plugins: [react()],
-    define: {
-      "process.env.VITE_API_BASE_URL": JSON.stringify(apiUrl)
-    }
-  };
-});
+export default defineConfig({
+  plugins: [react(),],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
